e5893940ea5801ec9532e8fb31a5ed452c75222d
Anatoliy2024 <gredlend20@gmail.com>
2024-10-19 02:52:37 +0300

    first commit

---
+++ function/getInfoFromServer.js
@@ -0,0 +1,18 @@
+fetch("http://localhost:5000/api/clinic")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok")
+    }
+    return response.json()
+  })
+  .then((clinics) => {
+    const table = document.querySelector(".table")
+    table.innerHTML = ""
+
+    clinics.forEach((clinic) => {
+      const row = document.createElement("tr")
+      row.innerHTML = `<td><a href="${clinic[0]}">${clinic[0]}</a></td><td>${clinic[1]}</td><td>${clinic[2]}</td>`
+      table.appendChild(row)
+    })
+  })
+  .catch((err) => console.error("Error:", err))
